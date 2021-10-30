dd18b271933c9a0b0947a85b7fb1e13afdac447d
diego-saurus <aldiegoid20019@gmail.com>
2021-10-30 20:03:41 +0800

    merge to custom-auth

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,20 @@
-import Layout from "components/Layout"
+import { NextPage } from "next"
 import Link from "next/link"
+
+import Layout from "components/Layout"
 import Ellipse from "svg/404 ellipse.svg"
 import Mountain from "svg/mountain.svg"
-import { NextPage } from "next"
+import MountainDesk from "svg/mountain_desktop.svg"
 
 const ErrorNotFound: NextPage = () => {
   return (
     <Layout title="404">
       <div className="centering overflow-x-hidden mt-36 flex flex-col items-center justify-between h-110 text-gold-lightest">
         <div className="flex flex-col items-center z-10">
-          <h1 className="text-8xl font-bold text-white flex items-center justify-between w-11/12">
-            4{<Ellipse className="inline w-18" />}4
+          <h1 className="text-8xl md:text-[9.5rem] font-bold text-white flex items-center justify-between">
+            4{<Ellipse className="inline w-18 md:w-24 mx-3 md:mx-5" />}4
           </h1>
-          <p className="font-bold text-xl text-center">
+          <p className="font-bold text-xl md:text-4xl text-center">
             You look like a little lost...
           </p>
         </div>
@@ -23,6 +25,7 @@ const ErrorNotFound: NextPage = () => {
         </Link>
       </div>
       <Mountain className="absolute z-0 -bottom-10 md:hidden" />
+      <MountainDesk className="absolute z-0 -bottom-10 hidden md:block" />
     </Layout>
   )
 }
