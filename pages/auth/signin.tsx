f268388ef4da5b2137ee9f9ffafb46d1fb1bf771
diego-saurus <aldiegoid20019@gmail.com>
2021-10-30 15:45:21 +0800

    singin usign Form Component

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { BuiltInProviderType } from "next-auth/providers"
-import { GetServerSideProps, NextPage } from "next"
-import { useRouter } from "next/router"
-import { getSession, LiteralUnion, signIn } from "next-auth/react"
+import { getSession, LiteralUnion, signIn, useSession } from "next-auth/react"
 
 import Layout from "components/Layout"
-import ArrowSvg from "svg/east_black_24dp.svg"
 import FacebookIcon from "svg/facebook_icon.svg"
 import GoogleIcon from "svg/google_icon.svg"
 import TwitterIcon from "svg/twitter_icon.svg"
-import { useMediaQuery } from "hooks/useMediaQuery"
-
-const Features = ["Like", "Favorite", "Add To Cart"]
+import { FormSubmit, Form, FormControl, Values } from "components/Form"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { GetServerSideProps } from "next"
 
 interface Prov {
   id: LiteralUnion<BuiltInProviderType, string>
@@ -41,134 +39,89 @@ const Provider: Prov[] = [
   },
 ]
 
-const SignIn: NextPage = () => {
-  const isMedia = useMediaQuery("md")
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req })
+  return session
+    ? {
+        redirect: {
+          destination: "/profile",
+          permanent: false,
+        },
+      }
+    : {
+        props: {},
+      }
+}
+
+const SignIn: React.FC = () => {
+  const [loading, setLoading] = useState(false)
+  const { push } = useRouter()
+
+  const handleSubmit = async (val: Values) => {
+    setLoading(true)
+    const { error } = await signIn("credentials", {
+      redirect: false,
+      ...val,
+    })
+    setLoading(false)
+    if (error) alert(error)
+    push("/profile")
+  }
+
   return (
-    <Layout title="Login">
-      <div className="flex flex-col py-10 text-white md:py-12 md:px-24 items-center">
-        <h1 className="text-3xl md:text-5xl font-bold text-gold-light text-center">
-          Login to Your Account
-        </h1>
-        <span className="mt-2 mb-10 md:my-5 text-white text-opacity-80 text-sm md:text-xl w-10/12 md:w-1/2 text-center font-medium">
-          Nikmati fitur-fitur seperti{" "}
-          {Features.map((m, i) => (
-            <span key={i} className="text-gold-light text-opacity-90">
-              {m},{" "}
-            </span>
-          ))}{" "}
-          dan lainnya dengan hanya login ke website ini
-        </span>
-        <div className="grid grid-cols-9 md:gap-x-10 w-10/12 md:w-8/12 items-center justify-center">
-          <div className="grid grid-flow-col col-span-full gap-x-2 items-center md:order-last md:gap-x-0 md:gap-y-5 md:grid-flow-row md:col-span-4">
-            {Provider.map((prov) =>
-              prov.id === "credentials" ? null : (
-                <ProviderComponent
-                  isMedia={isMedia}
-                  provider={prov}
-                  key={prov.id}
-                />
-              )
-            )}
+    <Layout title="Sign In">
+      <div className="text-white p-5">
+        <h1 className="text-2xl">Log in</h1>
+        <p className="text-white text-opacity-70 font-light my-3 text-sm">
+          log in with one of this following option
+        </p>
+        <div className="grid gap-y-8">
+          <div className="flex justify-between">
+            {Provider.map((p) => {
+              return p.id !== "credentials" ? (
+                <ProviderItem key={p.id} provider={p} />
+              ) : null
+            })}
           </div>
-          <span className="hidden md:inline text-3xl col-span-1 text-center self-center">
-            /
-          </span>
-          <CredentialForm className="grid md:order-first gap-x-2 gap-y-3 md:gap-x-0 md:gap-y-6 col-span-full md:col-span-4" />
+          <Form
+            onSubmit={handleSubmit}
+            initialValues={{ username: "", password: "" }}
+          >
+            <FormControl
+              type="text"
+              label="Username"
+              strict={true}
+              placeholder="Account Username"
+            />
+            <FormControl
+              type="password"
+              label="Password"
+              placeholder="Secure Password"
+            />
+            <FormSubmit>Log in</FormSubmit>
+          </Form>
         </div>
-        <a className="hover:underline cursor-pointer text-sm md:text-base my-5">
-          Forgot Password?
-        </a>
+        <p className="text-center text-white text-opacity-70 my-5">
+          Dont have Account?{" "}
+          <Link href="/auth/signup">
+            <a className="text-opacity-100 text-white underline">Sign Up</a>
+          </Link>
+        </p>
+        {loading && "loading"}
       </div>
     </Layout>
   )
 }
 
-const primaryClass =
-  "bg-gradient-to-br from-black-light to-black text-sm md:text-base py-3 md:py-4 px-6 focus:outline-none rounded-md"
-const CredentialForm: React.FC<{ className?: string }> = ({ className }) => {
-  const { push } = useRouter()
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const { error, ok } = await signIn("credentials", {
-      redirect: false,
-      username: usernameRef.current.value,
-      password: passwordRef.current.value,
-    })
-    if (error) return alert(error)
-    push("/profile")
-  }
-
-  const usernameRef = useRef<HTMLInputElement>(null)
-  const passwordRef = useRef<HTMLInputElement>(null)
-
-  return (
-    <form className={className ? className : ""} onSubmit={handleFormSubmit}>
-      <input
-        className={primaryClass}
-        placeholder="Username"
-        type="text"
-        ref={usernameRef}
-      />
-      <input
-        className={primaryClass}
-        placeholder="8 or More Digit Secure Password"
-        type="password"
-        ref={passwordRef}
-      />
-      <button>
-        <div className="font-bold text-sm md:text-base py-3 md:py-4 px-6 transition-all duration-500 ease-in-out focus:outline-none rounded-md cursor-pointer bg-gold-light hover:bg-gold-lightest text-black flex justify-between items-center">
-          Login to Your Account <ArrowSvg className="inline" />
-        </div>
-      </button>
-    </form>
-  )
-}
-
-const ProviderComponent: React.FC<{
-  provider: Prov
-  isMedia: boolean
-}> = ({ provider, isMedia }) => {
-  const handleSignIn = async (
-    prov: LiteralUnion<BuiltInProviderType, string>
-  ) => {
-    const { error, ok } = await signIn(prov, {
-      redirect: false,
-    })
-
-    if (error) alert(error)
-  }
-
+const ProviderItem: React.FC<{ provider: Prov }> = ({ provider }) => {
   return (
     <div
-      onClick={() => handleSignIn(provider.id)}
-      className="flex text-sm md:text-base items-center justify-start cursor-pointer active:bg-gold-lightest font-normal tracking-wide py-2 md:py-4 transition-all duration-150 ease-in-out px-5 bg-black rounded-md border-gold hover:border-gold-lightest border-[1px]"
+      className="px-11 py-1.5 transition-all ease-in-out duration-300 rounded-xl border-[1px] border-gold bg-black-light active:bg-gold-lightest"
+      // onClick={() => signIn(provider.id)}
     >
-      <provider.Icon className="inline fill-current text-white mx-5 h-7" />
-      {isMedia && <span>Sign in With {provider.name}</span>}
+      <provider.Icon className="h-6 fill-current" />
     </div>
   )
-}
-// export const getServerSideProps: GetServerSideProps = async ({
-//   req,
-// }: GetServerSidePropsContext): Promise<GetServerSidePropsResult<unknown>> => {
-//   const csrfToken = await getCsrfToken({ req })
-//   return {
-//     props: { csrfToken },
-//   }
-// }
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const session = await getSession({ req })
-  if (session)
-    return {
-      redirect: {
-        destination: "/profile",
-        permanent: false,
-      },
-    }
-  return {
-    props: {},
-  }
 }
 
 export default SignIn
